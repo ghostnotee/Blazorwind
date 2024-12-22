71d14834a604c78f8274a31eadad37d05d3dabc1
ghostNoté <selcukbilgen@hotmail.com>
2024-12-23 00:56:46 +0300

    post css minify added.

--- src/WebApp/Styles/tailwind.config.js
+++ src/WebApp/Styles/tailwind.config.js
@@ -1,31 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["../**/*.{razor,html,cshtml,scss}"],
-    theme: {
+    content: ["../**/*.{razor,html,cshtml,scss}"], theme: {
         container: {
-            center: true,
-            screens: {
-                lg: '1140px',
-                xl: '1140px',
-                '2xl': '1140px'
+            center: true, screens: {
+                lg: '1140px', xl: '1140px', '2xl': '1140px'
             }
-        },
-        extend: {
+        }, extend: {
             fontFamily: {
-                gemunu: ['Gemunu Libre', 'serif'],
-                open: ['Open Sans', 'serif'],
-            },
-            colors: {
-                'gega-red': '#BC1A45',
-                'gega-melon': '#FFD369',
-                'gega-grey': '#DDDDDD',
-                'gega-white': '#F7F7F7',
-            },
-            spacing: {
+                gemunu: ['Gemunu Libre', 'serif'], open: ['Open Sans', 'serif'],
+            }, colors: {
+                'gega-red': '#BC1A45', 'gega-melon': '#FFD369', 'gega-grey': '#DDDDDD', 'gega-white': '#F7F7F7',
+            }, spacing: {
                 '128': '32rem'
             }
         },
-    },
-    plugins: [],
-} 
-
+    }, plugins: [require('autoprefixer'), ...(process.env.NODE_ENV === 'production' ? [require('cssnano')] : []),],
+}
